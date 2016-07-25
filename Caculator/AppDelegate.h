25e462dc2cbf4f8863273517eab59a4269e77a22
创新创业中心 <cxcy@cxcy-imac.lan>
2016-07-25 17:03:18 +0800

    Initial Commit

---
+++ Caculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Caculator
+//
+//  Created by 创新创业中心 on 16/7/25.
+//  Copyright © 2016年 UESTCACM QKTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
